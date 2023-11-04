c2320b38e85c66e028eaf20efcaf7284e470dcf6
Roeland Kluit <roeland@kluit.net>
2023-11-04 16:31:01 +0100

    Bugfixes, added timeout counters and retry functionality

--- src/BuildConfig.h
+++ src/BuildConfig.h
@@ -2,8 +2,8 @@
 *
 * Title			    : Free-ESPatHome
 * Description:      : Library that implements the Busch-Jeager / ABB Free@Home API for ESP8266 and ESP32.
-* Version		    : v 0.4
-* Last updated      : 2023.10.29
+* Version		    : v 0.5
+* Last updated      : 2023.11.04
 * Target		    : ESP32, ESP8266, ESP8285
 * Author            : Roeland Kluit
 * Web               : https://github.com/roelandkluit/Free-ESPatHome
@@ -13,16 +13,16 @@
 
 #pragma once
 
-//#define DEBUG
+#define DEBUG
 //#define FORCE_ESP8266_SSL_OPTION_AVAILBLE
 #define MAX_ESP_CREATED_DEVICES 10
-#define WS_PING_INTERVAL_TIMEOUT	  20
+#define WS_PING_INTERVAL_TIMEOUT	  20000
 #define MAX_ARDUINOJSON_DOC_SIZE 10000
-
+#define HTTP_SESSION_TIMEOUT_MS 20000 //20 seconds session timeout for http requests
+//#define CHECK_WIFI_CONNECTION_BEFORE_SEND  //Not implemented
 
 //Shared function defines
 
-#define Seconds() (millis()/1000)
 
 #ifdef DEBUG
 #define DEBUG_PL Serial.println
--- src/FahESPWeatherStation.cpp
+++ src/FahESPWeatherStation.cpp
@@ -2,8 +2,8 @@
 *
 * Title			    : Free-ESPatHome
 * Description:      : Library that implements the Busch-Jeager / ABB Free@Home API for ESP8266 and ESP32.
-* Version		    : v 0.4
-* Last updated      : 2023.10.29
+* Version		    : v 0.5
+* Last updated      : 2023.11.04
 * Target		    : ESP32, ESP8266, ESP8285
 * Author            : Roeland Kluit
 * Web               : https://github.com/roelandkluit/Free-ESPatHome
